f04a84794a1f0f781aa078c30ebaaa7194a487b1
andremelinski <andremelinski29@gmail.com>
2024-02-29 15:55:42 -0300

    --amend

--- desafios/Client-Server-API/client/main.go
+++ desafios/Client-Server-API/client/main.go
@@ -10,20 +10,6 @@ import (
 	"github.com/andremelinski/pos-goexpert/desafios/Client-Server-API/client/document"
 )
 
-type UsdbrlBid struct {
-	Code       string `json:"code,omitempty"`
-	Codein     string `json:"codein,omitempty"`
-	Name       string `json:"name,omitempty"`
-	High       string `json:"high,omitempty"`
-	Low        string `json:"low,omitempty"`
-	VarBid     string `json:"varBid,omitempty"`
-	PctChange  string `json:"pctChange,omitempty"`
-	Bid        string `json:"bid,omitempty"`
-	Ask        string `json:"ask,omitempty"`
-	Timestamp  string `json:"timestamp,omitempty"`
-	CreateDate string `json:"create_date,omitempty"`
-}
-
 func main() {
 	ctx, cancel := context.WithTimeout(context.Background(), 300*time.Millisecond)
 	// ao final de tudo, vc fecha a req
