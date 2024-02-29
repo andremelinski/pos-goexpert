451fd1ac0b99c58caaf30f76a4f5e85d127f479b
andremelinski <andremelinski29@gmail.com>
2024-02-29 15:54:43 -0300

    fix client - adding O_WRONLY permission

--- desafios/Client-Server-API/client/document/document.go
+++ desafios/Client-Server-API/client/document/document.go
@@ -11,9 +11,9 @@ func DocumentInit(payload string) *Document {
 		payload,
 	}
 }
-
+//  https://stackoverflow.com/questions/33851692/golang-bad-file-descriptor
 func (d *Document) CreateFile() error {
-	filePointer, err := os.OpenFile("dataFromServer.txt", os.O_APPEND|os.O_CREATE, 0644)
+	filePointer, err := os.OpenFile("dataFromServer.txt", os.O_APPEND|os.O_CREATE | os.O_WRONLY, 0644)
 	if err != nil {
 		return err
 	}
