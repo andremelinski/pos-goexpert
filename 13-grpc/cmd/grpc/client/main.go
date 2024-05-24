6242429dbb462f276f49f3e7154db18ad5ff0590
andremelinski <andremelinski29@gmail.com>
2024-05-23 23:30:04 -0300

    client side on client stream

--- 13-grpc/cmd/grpc/client/main.go
+++ 13-grpc/cmd/grpc/client/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
+	"time"
 
 	"github.com/andremelinski/pos-goexpert/13-grpc/internal/pb"
 	"google.golang.org/grpc"
@@ -17,14 +19,13 @@ var (
 var opts []grpc.DialOption
 
 func main(){
-	
 	conn, err := grpc.NewClient(*addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 	  panic(err)
 	}
 
 	client := pb.NewCategoryServiceClient(conn)
-
+		
 	// newUser, err := client.CreateCategory(context.Background(), &pb.CreateCategoryRequest{Name:"random name", Description: "dsescrpition"})
 	// if err != nil {
 	// 	panic(err)
@@ -32,19 +33,37 @@ func main(){
 
 	// log.Println(newUser)
 
-	in := &pb.Blank{}
-	categories, err := client.ListCategory(context.Background(), in)
+	// in := &pb.Blank{}
+	// categories, err := client.ListCategory(context.Background(), in)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// log.Println(categories.Categories)
+	// id := &pb.CategoryIdRequest{Id:"2ee6e3c1-02fc-48ff-8b55-7ba878b98183"}
+	// category, err := client.GetCategory(context.Background(), id)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// log.Println(category)
+	// defer conn.Close()
+
+	stream, err := client.CreateCategoryStream(context.Background())
+	for i := 0; i < 10; i++ {
+		name := fmt.Sprintf("category from stream %d", i)
+		err := stream.Send(&pb.CreateCategoryRequest{Name: name, Description: "description"})
+		if err != nil {
+			log.Fatalln("Send stream",err)
+		}
+
+		time.Sleep(100*time.Millisecond)
+	}
+	res, err := stream.CloseAndRecv()
 	if err != nil {
-		panic(err)
+		log.Fatalln("CloseAndRecv stream",err)
 	}
 
-	log.Println(categories.Categories)
-	id := &pb.CategoryIdRequest{Id:"2ee6e3c1-02fc-48ff-8b55-7ba878b98183"}
-	category, err := client.GetCategory(context.Background(), id)
-	if err != nil {
-		panic(err)
-	}
-
-	log.Println(category)
+	fmt.Println(res.Categories)
 	defer conn.Close()
 }
