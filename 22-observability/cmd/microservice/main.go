ac2a793d994436af5a18f348de4a325ac9fd1005
andremelinski <andremelinski29@gmail.com>
2024-07-18 15:14:04 -0300

    observability

---
+++ 22-observability/cmd/microservice/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/andremelinski/observability/comunicacao-ms/internal/web"
+	"github.com/spf13/viper"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
+)
+
+// cara que faz toda a comunicacao e preparacao do open pra falar da aplicacao
+func initProvider(serviceName, collectorURL string) (func(context.Context) error, error) {
+	ctx := context.Background()
+
+	res, err := resource.New(ctx,
+		resource.WithAttributes(
+			semconv.ServiceName(serviceName),
+		),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create resource: %w", err)
+	}
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	defer cancel()
+	conn, err := grpc.DialContext(ctx, collectorURL,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithBlock(),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create gRPC connection to collector: %w", err)
+	}
+	// Exporter do trace -> exporta os dados por uma comunicacao gRPC (mas pode ser http) 
+	traceExporter, err := otlptracegrpc.New(ctx, otlptracegrpc.WithGRPCConn(conn))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create trace exporter: %w", err)
+	}
+	// will send completed span batches to the exporter
+	bsp := sdktrace.NewBatchSpanProcessor(traceExporter)
+	// vai pegar o recusro que foi criado no provider (res) e  SpanProcessor
+	//  ele que faz com que as infos sejam consolidadas e une todas as infos
+	tracerProvider := sdktrace.NewTracerProvider(
+		// WithSampler -> amostragem que a gente quer pra enviar o trace
+		// para dev a gente utiliza AlwaysSample -> para cada req envia a info
+		sdktrace.WithSampler(sdktrace.AlwaysSample()),
+		sdktrace.WithResource(res),
+		sdktrace.WithSpanProcessor(bsp),
+	)
+	otel.SetTracerProvider(tracerProvider)
+	// propaga a info do trace
+	otel.SetTextMapPropagator(propagation.TraceContext{})
+
+	return tracerProvider.Shutdown, nil
+}
+
+// load env vars cfg
+func init() {
+	viper.AutomaticEnv()
+}
+
+func main() {
+	// graceful shutdown
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+	// graceful shutdown acabada
+	// inicia o provider
+	shutdown, err := initProvider(viper.GetString("OTEL_SERVICE_NAME"), viper.GetString("OTEL_EXPORTER_OTLP_ENDPOINT"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		if err := shutdown(ctx); err != nil {
+			log.Fatal("failed to shutdown TracerProvider: %w", err)
+		}
+	}()
+	// agora que ja configuramos o trace e os servicos que vao exportar, iniciar de fato o trace que vai monitorar a aplicacao 
+	tracer := otel.Tracer("microservice-tracer")
+
+	templateData := &web.TemplateData{
+		Title:              viper.GetString("TITLE"),
+		BackgroundColor:    viper.GetString("BACKGROUND_COLOR"),
+		ResponseTime:       time.Duration(viper.GetInt("RESPONSE_TIME")),
+		ExternalCallURL:    viper.GetString("EXTERNAL_CALL_URL"),
+		ExternalCallMethod: viper.GetString("EXTERNAL_CALL_METHOD"),
+		RequestNameOTEL:    viper.GetString("REQUEST_NAME_OTEL"),
+		OTELTracer:         tracer, // acessa la no webserver o tracer pra mapear as coisas 
+	}
+	server := web.NewServer(templateData)
+	router := server.CreateServer()
+
+	go func() {
+		log.Println("Starting server on port", viper.GetString("HTTP_PORT"))
+		if err := http.ListenAndServe(viper.GetString("HTTP_PORT"), router); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	select {
+	case <-sigCh:
+		log.Println("Shutting down gracefully, CTRL+C pressed...")
+	case <-ctx.Done():
+		log.Println("Shutting down due to other reason...")
+	}
+
+	// Create a timeout context for the graceful shutdown
+	_, shutdownCancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdownCancel()
+}
